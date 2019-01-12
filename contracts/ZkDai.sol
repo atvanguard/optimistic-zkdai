aa55baf864c57ad85ba047cf6dddf4489d1a5d3b
atvanguard <93arpit@gmail.com>
2019-01-12 22:20:38 +0530

    liquidateNote

--- contracts/ZkDai.sol
+++ contracts/ZkDai.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.4.25;
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import "./CreateNotes.sol";
 import "./SpendNotes.sol";
+import "./LiquidateNotes.sol";
 
-contract ZkDai is CreateNotes {
+contract ZkDai is CreateNotes, SpendNotes, LiquidateNotes {
   ERC20 internal DAI_TOKEN_ADDRESS;
 
   constructor(uint256 _cooldown, uint256 _stake, address daiTokenAddress)
@@ -28,14 +29,29 @@ contract ZkDai is CreateNotes {
     payable {
       require(msg.value == stake, 'Invalid stake amount');
       // require(
-      //   DAI_TOKEN_ADDRESS.transfer(msg.sender, uint256(input[2]) /* value */),
+      //   DAI_TOKEN_ADDRESS.transferFrom(msg.sender, address(this), uint256(input[2]) /* value */),
       //   'daiToken transfer failed'
       // );
       CreateNotes.submit(a, a_p, b, b_p, c, c_p, h, k, input);
     }
 
-    function liquidateNote() {
-
+    function liquidateNote(
+        address to,
+        uint[2] a,
+        uint[2] a_p,
+        uint[2][2] b,
+        uint[2] b_p,
+        uint[2] c,
+        uint[2] c_p,
+        uint[2] h,
+        uint[2] k,
+        uint[4] input)
+      external {
+        LiquidateNotes.liquidateNote(a, a_p, b, b_p, c, c_p, h, k, input);
+        require(
+          DAI_TOKEN_ADDRESS.transfer(to, uint256(input[2]) /* value */),
+          'daiToken transfer failed'
+      );
     }
 
 }
